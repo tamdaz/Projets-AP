881a250e237f18f5cb1dded5e0c4b36b98f4fc68
tamdaz <tamdazohir01@gmail.com>
2024-02-08 13:59:44 +0100

    Premier commit

---
+++ AP 1/composants/header.php
@@ -0,0 +1,3 @@
+<header class="geekzone-header">
+    GeekZone
+</header>
---
+++ AP 1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Geekzone</title>
+    </head>
+    <body>
+        <?php include "./composants/header.php" ?>
+        ok
+        <?php include "./composants/footer.php" ?>
+    </body>
+</html>
