0bc94f5cffd305b0b42390ba4411af58b20a1ead
tamdaz <tamdazohir01@gmail.com>
2024-02-29 13:37:55 +0100

    Formater le code boutiques.php

--- AP 1/boutiques.php
+++ AP 1/boutiques.php
@@ -1,58 +1,25 @@
 <!DOCTYPE html>
-
 <html lang="fr">
-
-<head>
-
-    <meta charset="UTF-8" />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <link rel="stylesheet" href="/assets/css/index.css" />
-
-    <title>Geekzone</title>
-
-</head>
-
-<body>
-
-    <div class="page">
-
-        <?php include "./composants/header.php" ?>
-
-
-
-        <div class="geekzone-container">
-
-            <h1>Recherche</h1>
-
-
-            <img src="/assets/img/boutique_albertville.jpg" alt="Boutique Albertville">
-
-            <img src="/assets/img/boutique_annecy.jpg" alt="Boutique Annecy">
-
-            <img src="/assets/img/boutique_chambery.jpg" alt="Boutique Chambery">
-
-            <img src="/assets/img/boutique_clermont.jpeg" alt="Boutique Clermont">
-
-            <img src="/assets/img/boutique_grenoble.jpg" alt="Boutique Grenoble">
-
-            <img src="/assets/img/boutique_lyon.jpg" alt="Boutique Lyon">
-
-            <img src="/assets/img/boutique_valence.jpg" alt="Boutique Valence">
-
-
-
-
-
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/assets/css/index.css" />
+        <title>Geekzone</title>
+    </head>
+    <body>
+        <div class="page">
+            <?php include "./composants/header.php" ?>
+            <div class="geekzone-container">
+                <h1>Recherche</h1>
+                <img src="/assets/img/boutique_albertville.jpg" alt="Boutique Albertville" />
+                <img src="/assets/img/boutique_annecy.jpg" alt="Boutique Annecy" />
+                <img src="/assets/img/boutique_chambery.jpg" alt="Boutique Chambery" />
+                <img src="/assets/img/boutique_clermont.jpeg" alt="Boutique Clermont" />
+                <img src="/assets/img/boutique_grenoble.jpg" alt="Boutique Grenoble" />
+                <img src="/assets/img/boutique_lyon.jpg" alt="Boutique Lyon" />
+                <img src="/assets/img/boutique_valence.jpg" alt="Boutique Valence" />
+            </div>
+            <?php include "./composants/footer.php" ?>
         </div>
-
-
-
-        <?php include "./composants/footer.php" ?>
-
-    </div>
-
-</body>
-
+    </body>
 </html>
