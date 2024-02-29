12b286e8c7a05a7c214fcc5546d51b55e663c112
tamdaz <tamdazohir01@gmail.com>
2024-02-29 17:14:08 +0100

    MAJ données

--- AP 1/données/produits.php
+++ AP 1/données/produits.php
@@ -2,10 +2,37 @@
 
 return [
     [
+        "id" => 1,
         "nom" => "Glaçons Tetris",
-        "description" => "Plongez ces glaçons Tetris dans votre verre et ceux de vos amis pour recréer des parties interminables ! Un élément geek indispensable pour passer une bonne soirée à se remémorer les jeux de votre enfance ! Regardez les glaçons s''imbriquer les uns sur les autres tout en buvant votre breuvage bien frais.",
-        "detail" => "LICENCE OFFICIELLE TETRIS\r\n<br/>\r\nRecréez une partie de Tetris mais cette fois-ci dans votre verre avec ces glaçons Tetris !<br/>\r\nSous licence officielle, ces glaçons emblématiques sont les véritables glaçons à forme de blocs Tetris : les Tetrimono !<br/>\r\nPlongez-les dans les verres de vos amis pour les ramener au bon vieux temps des jeux d''arcade !<br/>\r\n<br/>\r\nCaractéristiques :<br/>\r\n* Bac à glaçons en plastique mou, il reste souple même quand les glaçons sont gelés afin que votre glaçon en forme de tetrimono soit toujours facile à enlever.",
+        "description" => "Plongez ces glaçons Tetris dans votre verre et ceux de vos amis pour recréer des parties interminables ! Un élément geek indispensable pour passer une bonne soirée à se remémorer les jeux de votre enfance ! Regardez les glaçons s\'imbriquer les uns sur les autres tout en buvant votre breuvage bien frais.",
+        "detail" => "LICENCE OFFICIELLE TETRIS<br/>Recréez une partie de Tetris mais cette fois-ci dans votre verre avec ces glaçons Tetris !<br/>Sous licence officielle, ces glaçons emblématiques sont les véritables glaçons à forme de blocs Tetris : les Tetrimono !<br/>Plongez-les dans les verres de vos amis pour les ramener au bon vieux temps des jeux d\'arcade !<br/><br/>Caractéristiques :<br/>* Bac à glaçons en plastique mou, il reste souple même quand les glaçons sont gelés afin que votre glaçon en forme de tetrimono soit toujours facile à enlever.",
         "prix" => 8.9,
-        "image" => "glacons-tetris.jpg"
+        "image" => "glacons-tetris.jpg",
+        "categorie" => "cuisine"
+    ], [
+        "id" => 2,
+        "nom" => "Mug Pac-Man chaud/froid",
+        "description" => "Fan de Pac-Man, ce mug est fait pour vous ! Froid, c\'est une grille de jeu Pac-Man ... Chaud, Pac-Man, les fantômes et les Pac-Gommes apparaissent !",
+        "detail" => "LICENCE OFFICIELLE PAC-MAN<br/><br/>Fan de Pac-Man, ce mug est fait pour vous !<br/><br/>Froid, c'est une grille de jeu Pac-Man ...<br/><br/>Chaud, Pac-Man, les fantômes et les Pac-Gommes apparaissent !<br/><br/>Attention : Ne pas laver au lave-vaisselle ni utiliser au micro-onde.",
+        "prix" => 9.9,
+        "image" => "fridgebook-magnet-reseau-social-frigo.jpg",
+        "categorie" => "cuisine"
+    ], [
+        "id" => 3,
+        "nom" => "Magnets fridgebook",
+        "description" => "Recréez l\'environnement de votre réseau social préféré ... sur votre frigo ! grâce à nos magnets fridgebook. Mettez à jour votre statut et commentaires à l\'aide du stylo feutre fourni.",
+        "detail" => "Recréez votre réseau social préféré ... sur votre frigo ! grâce à nos magnets Fridgebook !<br/><br/>Mettez à jour votre statut, postez des commentaires et faites de votre wall le défouloir de la maison !<br/><br/>Ce kit pour reproduire un réseau social est livré avec un stylo feutre pour écrire facilement ainsi qu\'une mini-brosse pour vite effacer les commentaires et en écrire de nouveaux.<br/><br/>De nombreux magnets sont inclus dans le pack :<br/><br/>* 1 x Statuts<br/>* 2 x Comment<br/>* 2 x Wall<br/>* 2 x Friends (à personnaliser avec des photos de vos amis)<br/>* 5 Logos (Photos, Events, Notes, Like, Dislike).",
+        "prix" => 14.9,
+        "image" => "souris-optique-pixel.jpg",
+        "categorie" => "gadget"
     ]
 ];
+
+// [
+//     "id" => 1,
+//     "nom" => "",
+//     "description" => "",
+//     "detail" => "",
+//     "prix" => 0.0,
+//     "image" => ""
+// ];
