8ee869dc6191e3b233bc286e9b9d072d8baefce1
tamdaz <tamdazohir01@gmail.com>
2024-03-14 15:54:30 +0100

    Amélioration du recherche de produits

--- AP 1/produits.php
+++ AP 1/produits.php
@@ -10,44 +10,20 @@
     <body>
         <?php $produits = require "./données/produits.php" ?>
         <?php $categories = require "./données/categories.php" ?>
+        <?php $villes = require "./données/villes.php" ?>
+
         <div class="page">
             <?php include "./composants/header.php" ?>
             <div style="background-color: #00E676; width: 100%; height: 200px"></div>
-            <form class="geekzone-search-box" method="GET">
-                <input
-                    class="geekzone-input"
-                    type="text"
-                    placeholder="Que voulez-vous chercher ?"
-                    name="nom"
-                />
-                <select name="categorie" class="geekzone-input">
-                    <option default value="">Filtrer par catégorie</option>
-                    <?php foreach ($categories as $category): ?>
-                        <option value="<?= $category['libelle'] ?>">
-                            <?= $category['libelle'] ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <select class="geekzone-input">
-                    <option default>Filtrer par boutique</option>
-                </select>
-                <button type="submit" class="geekzone-button">Rechercher</button>
-            </form>
+            <?php include "./inclure/recherche.php" ?>
             <div class="geekzone-container">
                 <?php include "./composants/produit.php"; ?>
-
-                <?php
-                    $produits_filtres = array_filter($produits, function ($val) use ($categories) {
-						return str_starts_with(
-							strtolower($val['nom']), strtolower($_GET['nom'])
-						) || $val['categorie'] === intval($_GET['categorie']);
-					});
-                ?>
+                <?php include "./inclure/fonctions/filtre.php" ?>
 
                 <?php if (count($produits_filtres) !== 0): ?>
                     <div class="geekzone-grid-3">
                         <?php foreach ($produits_filtres as $produit): ?>
-							<?php echo creerProduit($produit); ?>
+                            <?php echo creerProduit($produit); ?>
                         <?php endforeach; ?>
                     </div>
                 <?php else: ?>
