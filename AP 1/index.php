bdb63db250385d5f0c535d89409d8b0fc1db76c4
sharma <sharma.mouttou.pro@gmail.com>
2024-03-21 15:18:35 +0100

    Page d'accueil

--- AP 1/index.php
+++ AP 1/index.php
@@ -13,6 +13,31 @@
             <?php include "./composants/header.php" ?>
             <div class="geekzone-container">
                 <h1>Bienvenue sur Geekzone</h1>
+                <html>  
+  <head>
+      <title>
+            Geekzone
+      </title>                                  
+  <style>
+body{
+ background-color:black;
+}
+</style>  
+   </head>
+
+    <body>
+<center>   
+
+  <h1>
+       <font color="#00FF16">Geekzone</font>
+  </h1>     
+<hr color="white" size="10"></hr>
+  
+  <font color="white" size="4pt">PLONGE DANS L'UNIVERS DES JEUX VIDEO<br>
+                                ET<br>
+                                 VIS DES AVENTURES INCROYABLES AVEC NOS SUPER EQUIPEMENTS</font>
+
+<center><img src="GeekZone-logo.png"></center>
             </div>
             <?php include "./composants/footer.php" ?>
         </div>
